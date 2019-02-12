21807b7e99d066fdbf8d02dee8d172ebf470c7a1
ReuvenLiran <reulir6@gmail.com>
2019-02-12 21:26:25 +0200

    create steps list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'codefresh';
+export class AppComponent implements OnInit {
+  title = 'test1';
+  steps = [];
+  ngOnInit() {
+    const steps = [ {
+      name: 'ssss',
+      status: 'success',
+    }, { 
+      name: 'main_clone',
+      status: 'success',
+    }, {
+      name: 'aaaaa',
+      status: 'error',
+    }];
+      for (let i = 0; i < steps.length; i++) {
+        setTimeout(() =>
+        this.steps.push(steps[i]), (i + 1) * 500)
+      }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StepComponent } from './step/step.component';
+import { StepsListComponent } from './steps-list/steps-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StepsListComponent,
     StepComponent,
   ],
   imports: [
